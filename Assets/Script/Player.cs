c727e35d3d1227b54ee66d36e6ccbcb12b62a272
Leo Langer <leolanger2001@outlook.com>
2023-07-08 13:44:43 +0800

    player controller

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -37,21 +37,37 @@ public class Player : Singleton<Player>
     {
         rig.MovePosition(this.transform.position + input * speed * Time.deltaTime);
         Quaternion target = this.transform.rotation;
-        if (input.x > 0 && input.x > Mathf.Abs(input.z))
+        if (input.x > 0 && input.z == 0)
         {
             target = Quaternion.Euler(new Vector3(0, 90, 0));
         }
-        else if (input.x < 0 && Mathf.Abs(input.x) > Mathf.Abs(input.z))
+        else if (input.x < 0 && input.z == 0)
         {
             target = Quaternion.Euler(new Vector3(0, -90, 0));
         }
-        else if(input.z > 0 && input.z > Mathf.Abs(input.x))
+        else if (input.z > 0 && input.x == 0)
         {
             target = Quaternion.Euler(new Vector3(0, 0, 0));
         }
-        else if(input.z < 0 && Mathf.Abs(input.z) > Mathf.Abs(input.x))
+        else if (input.z < 0 && input.x == 0)
         {
             target = Quaternion.Euler(new Vector3(0, 180, 0));
+        }
+        else if (input.x > 0 && input.z > 0)
+        {
+            target = Quaternion.Euler(new Vector3(0, 45, 0));
+        }
+        else if (input.x > 0 && input.z < 0)
+        {
+            target = Quaternion.Euler(new Vector3(0, -135, 0));
+        }
+        else if(input.x < 0 && input.z > 0)
+        {
+            target = Quaternion.Euler(new Vector3(0, -45, 0));
+        }
+        else if(input.x < 0 && input.z < 0)
+        {
+            target = Quaternion.Euler(new Vector3(0, 225, 0));
         }
         TurnAround(target);
     }
