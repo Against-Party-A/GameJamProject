b178312ef953d0be95fe70f3fb2b634f2d8d9926
MelodyBlue <bluesky4269@outlook.com>
2023-07-08 14:08:33 +0800

    儿童移动逻辑

---
+++ Assets/Script/PointMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointMove : MonoBehaviour
+{
+
+    public Vector3 pointA;
+
+    public Vector3 pointB;
+
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
