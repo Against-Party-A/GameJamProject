1457a649e2e63c4708fb823a4ecdbfe0b50ed082
Leo Langer <leolanger2001@outlook.com>
2023-07-08 21:55:42 +0800

    reset

--- Assets/Script/Settings.cs
+++ Assets/Script/Settings.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Settings
 {
-    public const float angerThreshold = 1f;    //经历多久时间怒气值加1.
-    public const float relieveThreshold = 0.25f;    //经历多久时间怒气值减1.
+    public const float angerThreshold = 0.25f;    //经历多久时间怒气值加1.
+    public const float relieveThreshold = 0.1f;    //经历多久时间怒气值减1.
 
     public const int prepareTime = 10;  //准备时间
 }
