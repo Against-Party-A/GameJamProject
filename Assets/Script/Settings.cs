2e51c12bc4d1c1fcecfde9064c8167275cc35b0c
Leo Langer <leolanger2001@outlook.com>
2023-07-09 15:44:29 +0800

    time update

--- Assets/Script/Settings.cs
+++ Assets/Script/Settings.cs
@@ -7,5 +7,5 @@ public class Settings
     public const float angerThreshold = 0.15f;    //经历多久时间怒气值加1.
     public const float relieveThreshold = 0.07f;    //经历多久时间怒气值减1.
 
-    public const int prepareTime = 10;  //准备时间
+    public const int prepareTime = 50;  //准备时间
 }
